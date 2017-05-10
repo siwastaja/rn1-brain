c7de16506f4e0df8d7eef518d7a17f7520944108
Antti Alhonen <antti.alhonen@iki.fi>
2017-05-10 14:11:41 +0300

    Sonar order fixed

--- sonar.h
+++ sonar.h
@@ -3,9 +3,9 @@
 
 #define SONAR_PULSE_ON()  {GPIOD->BSRR = 1UL<<3;}
 #define SONAR_PULSE_OFF() {GPIOD->BSRR = 1UL<<(3+16);}
-#define SONAR1_ECHO() (GPIOD->IDR & (1UL<<2))
+#define SONAR1_ECHO() (GPIOC->IDR & (1UL<<12))
 #define SONAR2_ECHO() (GPIOD->IDR & (1UL<<0))
-#define SONAR3_ECHO() (GPIOC->IDR & (1UL<<12))
+#define SONAR3_ECHO() (GPIOD->IDR & (1UL<<2))
 //#define SONAR4_ECHO() (GPIOD->IDR & (1UL<<1))
 
 #define MAX_NUM_SONARS 3
